df82259b9e333b8802577b698890ec26ebf269f2
joshua.ajose <josieboy55@gmail.com>
2021-08-17 14:49:53 +0100

    Added text to users table

--- database/migrations/2014_10_12_000001_create_users_table.php
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->text('location');
+            $table->longText('location');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
